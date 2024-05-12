95d1c1706af4fb6d06f357b2ff30e4b7f93aeea3
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-05-12 15:27:06 +0800

    c

---
+++ sundials/scripts/format.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------------
+# SUNDIALS Copyright Start
+# Copyright (c) 2002-2024, Lawrence Livermore National Security
+# and Southern Methodist University.
+# All rights reserved.
+#
+# See the top-level LICENSE and NOTICE files for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SUNDIALS Copyright End
+# ---------------------------------------------------------------------------------
+# This script will use clang-tidy and clang-format to format code.
+#
+# Usage:
+#    ./format.sh <paths to directories or files to format>
+#
+# We require clang-format 17.0.4. Other versions may produce different styles!
+# ---------------------------------------------------------------------------------
+
+if [ $# -lt 1 ]; then
+    echo "ERROR: At least one path to format required"
+    exit 1
+fi
+
+paths=( "$@" )
+
+find "${paths[@]}" -iname '*.h' -o -iname '*.hpp' -o \
+  -iname '*.c' -o -iname '*.cpp' -o \
+  -iname '*.cuh' -o -iname '*.cu' | grep -v fmod | xargs clang-format -i
