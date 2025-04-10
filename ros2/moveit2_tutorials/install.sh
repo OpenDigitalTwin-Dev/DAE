c9eb851bcd537b90ef17e683e4c8ee29145a8685
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-04-10 14:50:02 +0800

    c

--- ros2/moveit2_tutorials/install.sh
+++ ros2/moveit2_tutorials/install.sh
@@ -1,3 +1,8 @@
+source /opt/ros/jazzy/setup.bash
+
 rosdep install -r --from-paths src --ignore-src --rosdistro jazzy -y
 
 colcon build --event-handlers desktop_notification- status- --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+source install/setup.sh
+ros2 launch moveit2_tutorials demo.launch.py
